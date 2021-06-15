fb81a30b4166a70ca084186d62f31ac5a7dd9f76
yuchengli <yuchengli@tencent.com>
2021-06-15 14:50:23 +0800

    venus

--- rerank_after_recall.py
+++ rerank_after_recall.py
@@ -8,18 +8,20 @@ import sys
 import torch
 from torch.utils.data import DataLoader
 
-def rerank_eval_one_run(query, st_querys, tokenizer, model, topk=1):
+def rerank_eval_one_run(query, st_querys, tokenizer, model):
+#     print(len(st_querys))
     inputs = [[query]+[i] for i in st_querys]
-    encoding = tokenizer(inputs, truncation=True, padding=True)
+    encoding = tokenizer(inputs, truncation=True, padding=True, return_tensors='pt')
     ds = RerankEval(encoding)
     dl=DataLoader(ds, batch_size=64)
 
     for batch in dl:
         batch = to_gpu(batch)
-        output=model(*batch)
+        output=model(**batch)
     
     logits = output.logits
     prob = torch.softmax(logits, dim=-1)
+#     print(prob)
     positive_prob = prob[:, -1]
 
     return positive_prob
@@ -41,7 +43,7 @@ class OneRun:
         return r
     
     def in_topk(self, topk):
-        return any(answer[0] in self.gold for answer in getattr(self, 'top'+str(topk)))
+        return any(answer in self.gold for answer in getattr(self, 'top'+str(topk)))
     
     def set_answer(self, idx, topk):
         setattr(self, 'top'+str(topk), [self.recall[i] for i in idx])
@@ -56,7 +58,7 @@ class EvalSession:
     
     def get_topk(self, topks=[1, 3, 5]):
         for run in self.runs:
-            positive_prob = rerank_eval_one_run(run.query, run.recall, self.tokenizer, self.model, topk=topk)
+            positive_prob = rerank_eval_one_run(run.query, run.recall, self.tokenizer, self.model)
             for topk in topks:
                 values, index = positive_prob.topk(topk)
                 run.set_answer(index, topk)
@@ -73,17 +75,19 @@ class EvalSession:
         r={}
         for topk in topks:
             positive = np.array([run.in_topk(topk) for run in self.runs])
-            acc = positive/len(self.runs)
+            acc = positive.sum()/len(self.runs)
             r[topk]=acc
-        print(r)
+        return r
 
 if __name__ == '__main__':
 
     recall_json, model_path, = sys.argv[1:]
 
-    model_path='checkpoints/simcse3/checkpoint-5500/'
+    model_path='checkpoints/rerank/checkpoint-5000'
     model = get_model(rerank, model_path)
     tokenizer = get_tokenizer(model_path)
 
     eval=EvalSession(recall_json, tokenizer, model)
     print('ACC: ', eval.acc())
+    eval.output(['top1', 'top3'], 'r.json')
+
